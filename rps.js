b5d162e97457f68aede776f594e875a27389e70e
danielcr0995 <daniel.correa.1995@gmail.com>
2021-07-03 17:24:12 -0500

    function for computer and player choce created

--- rps.js
+++ rps.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+console.log("Rock, paper, scissors");
+
+
+
+console.log(computerPlay());
+console.log(playerSelection());
+
+
+// computer play
+
+function computerPlay(){
+    let choice;
+    let choices=['rock','paper','scissors'];
+    
+    let n =Math.floor(Math.random() *3);
+
+    choice=choices[n];
+    return choice;
+
+}
+
+//my play
+function playerSelection() {
+    let choicep;
+    // let choices=['rock (1)','paper(2)','scissors (3)'];
+    // let mychoice= promt('What is your choice? \n rock (1)','paper(2)','scissors (3)');
+    let mychoice= prompt('What is your choice?');
+    choicep=mychoice.toLowerCase();
+    return choicep;
+}
+
+// compare
