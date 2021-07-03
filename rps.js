f7aec72187fb6604c032df2615665892d7d271b1
danielcr0995 <daniel.correa.1995@gmail.com>
2021-07-03 17:53:54 -0500

    function game created

--- rps.js
+++ rps.js
@@ -1,6 +1,8 @@
 console.log("Rock, paper, scissors");
 
-playRound(computerPlay(),playerSelection())
+game();
+
+// playRound(computerPlay(),playerSelection())
 // for (let i=1;i<=5;i++){
 //     console.log(computerPlay());
 //     console.log(playerSelection());
@@ -38,20 +40,26 @@ function playRound(computer,player){
     if (computer===player){
         console.log("It's a tie")
     }else if( player==='rock' && computer==='scissors' ){
-        console.log("You won!!! Rock beats Scissors")
+        console.log("You won!!! Rock beats Scissors");
     }else if( player==='rock' && computer==='paper' ){
-        console.log("You lost :( Paper beats Rock")
+        console.log("You lost :( Paper beats Rock");
 
     }else if( player==='scissors' && computer==='paper' ){
         console.log("You won!!! scissors beats paper")
     }else if( player==='scissors' && computer==='rock' ){
-        console.log("You lost :( rock beats scissors")
+        console.log("You lost :( rock beats scissors");
 
     }else if( player==='paper' && computer==='rock' ){
-        console.log("You won!!! paper beats rock")
+        console.log("You won!!! paper beats rock");
     }else if( player==='paper' && computer==='scissors' ){
-        console.log("You lost :( scissors beats paper")
+        console.log("You lost :( scissors beats paper");
     }
 
+}
+
+function game(){
+    for (let i=1; i<=5;i++){
+        playRound(computerPlay(),playerSelection());
+    }
 }
 
